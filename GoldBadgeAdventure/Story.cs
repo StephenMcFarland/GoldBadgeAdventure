6fa0193715d6e64fee03ef30f96a1c65f7634b8b
stephenmcfarland <snmcfarland@gmail.com>
2019-03-22 14:47:44 -0400

    initial commit

---
+++ GoldBadgeAdventure/Story.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerProgramAdventure
+{
+    public class Story
+    {
+        public int Life { get; set; }
+        public int ProjectedSalary { get; set; }
+    }
+}
---
+++ StoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerProgramAdventure
+{
+    class StoryRepository
+    {
+
+        public int Life = new int();
+
+        public int MakeRandomNumberAndIncreaseOrDecreaseLife(int first, int second, bool increaseOrDecrease)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(first, second);
+            if (increaseOrDecrease)
+            {
+                Life += number;
+            }
+            else
+            {
+                Life -= number;
+            }
+
+
+
+            return Life;   
+
+        }
+    }
+}
